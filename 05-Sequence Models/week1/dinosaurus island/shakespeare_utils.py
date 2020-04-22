7ae80da79d92e099b0c222d145574553c3c82a9a
ulysses <ulysses1171@163.com>
2020-04-22 21:46:21 +0800

    shakespeare

--- 05-Sequence Models/week1/dinosaurus island/shakespeare_utils.py
+++ 05-Sequence Models/week1/dinosaurus island/shakespeare_utils.py
@@ -1,15 +1,17 @@
 # Load Packages
-from __future__ import print_function
-from keras.callbacks import LambdaCallback
-from keras.models import Model, load_model, Sequential
-from keras.layers import Dense, Activation, Dropout, Input, Masking
-from keras.layers import LSTM
-from keras.utils.data_utils import get_file
-from keras.preprocessing.sequence import pad_sequences
-import numpy as np
-import random
-import sys
-import io
+# from __future__ import print_function
+# from keras.callbacks import LambdaCallback
+# from keras.models import Model, load_model, Sequential
+# from keras.layers import Dense, Activation, Dropout, Input, Masking
+# from keras.layers import LSTM
+# from keras.utils.data_utils import get_file
+# from keras.preprocessing.sequence import pad_sequences
+# import numpy as np
+# import random
+# import sys
+# import io
+
+from tensorflow.keras import layers
 
 def build_data(text, Tx = 40, stride = 3):
     """
